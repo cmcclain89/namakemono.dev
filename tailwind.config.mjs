7238fb13afc213982890e3d9eb2a559d954071a1
Caleb <caleb.mcclain89@gmail.com>
2025-03-15 16:23:03 +0900

    before I make major changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        sligoo: {
+          background: {
+            DEFAULT: "#f7e6ff",
+          },
+          text: {
+            DEFAULT: "#807280",
+          },
+        },
+      },
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
